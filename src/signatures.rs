d4f9c99d4570ed9971f4c465f968dba6d9c97037
Michele Bertasi <michele.bertasi@gmail.com>
2015-10-04 09:25:55 +0000

    Refactoring signatures

--- src/signatures.rs
+++ src/signatures.rs
@@ -1,18 +1,18 @@
-use std::io;
+use std::io::{self, Read};
 use std::iter::Iterator;
 use std::slice;
-use std::path::Component;
+use std::path::{Component, PathBuf};
 
 use flate2::read::GzDecoder;
 use tar;
 use time::Timespec;
 
-use collections::{CollectionsStatus, SignatureChain, SignatureFile};
 use backend::Backend;
+use collections::{CollectionsStatus, SignatureChain, SignatureFile};
 
 
 pub struct BackupFiles {
-    snapshots: Vec<Snapshot>
+    chains: Vec<Chain>
 }
 
 pub struct Snapshot {
@@ -34,6 +34,32 @@ enum DiffType {
     Deleted
 }
 
+/// Store separately informations about the signatures and informations about the paths in the
+/// signatures. This allows to reuse informations between snapshots and avoid duplicating them.
+struct Chain {
+    timestamps: Vec<Timespec>,
+    files: Vec<PathSnapshots>
+}
+
+struct PathSnapshots {
+    // the directory or file path
+    path: PathBuf,
+    // all the snapshots for this path
+    snapshots: Vec<PathSnapshot>
+}
+
+struct PathSnapshot {
+    // info are None if the snapshot has deleted this path
+    info: Option<PathInfo>,
+    // the index of the snapshot in the chain
+    index: u8
+}
+
+/// Informations about a path inside a snapshot.
+struct PathInfo {
+    mtime: Timespec
+}
+
 
 impl BackupFiles {
     pub fn new<B: Backend>(backend: &B) -> io::Result<BackupFiles> {
@@ -41,39 +67,51 @@ impl BackupFiles {
             let filenames = try!(backend.get_file_names());
             CollectionsStatus::from_filenames(&filenames)
         };
-        let chains = collection.signature_chains();
-        for chain in chains {
-            let file = try!(backend.open_file(chain.fullsig.file_name.as_ref()));
-            if chain.fullsig.compressed {
-                let gz_decoder = try!(GzDecoder::new(file));
-                let mut tar = tar::Archive::new(gz_decoder);
-                for tarfile in try!(tar.files_mut()) {
-                    if let Ok(tarfile) = tarfile {
-                        let header = tarfile.header();
-                        let path = unwrap_or_continue!(header.path());
-                        let mut pcomps = path.components();
-                        // split the path in (first directory, the remaining path)
-                        // the first is the type, the remaining is the real path
-                        let pfirst = unwrap_opt_or_continue!(pcomps.next());
-                        if let Component::Normal(strfirst) = pfirst {
-                            let difftype = match strfirst.to_str() {
-                                Some("signature") => DiffType::Signature,
-                                Some("snapshot")  => DiffType::Snapshot,
-                                Some("deleted")   => DiffType::Deleted,
-                                _                 => { continue; }
-                            };
-                            let realpath = pcomps.as_path();
-                        }
-                    }
-                }
+        let mut chains: Vec<Chain> = Vec::new();
+        let coll_chains = collection.signature_chains();
+        for coll_chain in coll_chains {
+            let mut chain = Chain{ timestamps: Vec::new(), files: Vec::new() };
+            let file = try!(backend.open_file(coll_chain.fullsig.file_name.as_ref()));
+            add_sigfile_to_chain(&mut chain, file, &coll_chain.fullsig);
+            for inc in coll_chain.inclist.iter() {
+                let file = try!(backend.open_file(inc.file_name.as_ref()));
+                add_sigfile_to_chain(&mut chain, file, &inc);
             }
+            chains.push(chain);
         }
-        // TODO: go from signature chains to snapshots
-        Ok(BackupFiles{ snapshots: Vec::new() })
+        Ok(BackupFiles{ chains: chains })
     }
 
     pub fn snapshots(&self) -> Snapshots {
-        self.snapshots.iter()
+        unimplemented!()
     }
+}
+
+
+fn add_sigfile_to_chain<R: Read>(chain: &mut Chain, file: R, sigfile: &SignatureFile) -> io::Result<()> {
+    if sigfile.compressed {
+        let gz_decoder = try!(GzDecoder::new(file));
+        let mut tar = tar::Archive::new(gz_decoder);
+        for tarfile in try!(tar.files_mut()) {
+            if let Ok(tarfile) = tarfile {
+                let header = tarfile.header();
+                let path = unwrap_or_continue!(header.path());
+                let mut pcomps = path.components();
+                // split the path in (first directory, the remaining path)
+                // the first is the type, the remaining is the real path
+                let pfirst = unwrap_opt_or_continue!(pcomps.next());
+                if let Component::Normal(strfirst) = pfirst {
+                    let difftype = match strfirst.to_str() {
+                        Some("signature") => DiffType::Signature,
+                        Some("snapshot")  => DiffType::Snapshot,
+                        Some("deleted")   => DiffType::Deleted,
+                        _                 => { continue; }
+                    };
+                    let realpath = pcomps.as_path();
+                }
+            }
+        }
+    }
+    Ok(())
 }
 
