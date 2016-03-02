8960f819061c9fa7e63685f21753a8b488750f18
Michele Bertasi <michele.bertasi@gmail.com>
2016-03-02 20:41:52 +0100

    Working on UnsafeList

--- src/read/unslist.rs
+++ src/read/unslist.rs
@@ -17,7 +17,7 @@ pub struct Node<T> {
 
 
 type Link<T> = Option<Box<Node<T>>>;
-type RawLink<T> = Option<Shared<Node<T>>>;
+struct RawLink<T>(Option<Shared<Node<T>>>);
 
 
 // similar to std::ptr::Shared that is however unstable
@@ -33,7 +33,7 @@ impl<T> UnsafeList<T> {
         UnsafeList {
             length: 0,
             head: None,
-            tail: None,
+            tail: RawLink::none(),
         }
     }
 
@@ -41,14 +41,31 @@ impl<T> UnsafeList<T> {
         let mut new_head = Box::new(Node::new(val));
         match self.head {
             None => {
+                self.head = link_no_prev(new_head);
+                self.tail = RawLink::from_link(&mut self.head);
             }
             Some(ref mut head) => {
-                new_head.prev = None;
-                //head.prev = raw_link_from_link(&mut self.head);
+                new_head.prev = RawLink::none();
+                head.prev = RawLink::some(&mut *new_head);
                 mem::swap(head, &mut new_head);
                 head.next = Some(new_head);
             }
         }
+        self.length += 1;
+    }
+
+    pub fn push_back(&mut self, val: T) {
+        match unsafe { self.tail.resolve_mut() } {
+            None => {
+                return self.push_front(val);
+            }
+            Some(tail) => {
+                let mut new_tail = Box::new(Node::new(val));
+                tail.set_next(new_tail);
+                self.tail = RawLink::from_link(&mut tail.next);
+            }
+        }
+        self.length += 1;
     }
 }
 
@@ -57,16 +74,21 @@ impl<T> Node<T> {
     fn new(val: T) -> Self {
         Node {
             next: None,
-            prev: None,
+            prev: RawLink::none(),
             value: val,
         }
+    }
+
+    fn set_next(&mut self, mut next: Box<Node<T>>) {
+        debug_assert!(self.next.is_none());
+        next.prev = RawLink::some(self);
+        self.next = Some(next);
     }
 }
 
 
-
 impl<T: ?Sized> Shared<T> {
-    pub unsafe fn new(ptr: *mut T) -> Self {
+    unsafe fn new(ptr: *const T) -> Self {
         Shared {
             pointer: ptr,
             _marker: PhantomData,
@@ -83,7 +105,26 @@ impl<T: ?Sized> Deref for Shared<T> {
 }
 
 
-fn raw_link_from_link<T>(link: &mut Link<T>) -> RawLink<T> {
-    unimplemented!()
-    //link.as_mut().map(|bnode| unsafe { Shared::new(&mut bnode) })
+impl<T> RawLink<T> {
+    fn none() -> Self {
+        RawLink(None)
+    }
+
+    fn some(link: &Node<T>) -> Self {
+        RawLink(unsafe { Some(Shared::new(mem::transmute(link))) })
+    }
+
+    fn from_link(link: &Link<T>) -> Self {
+        RawLink(link.as_ref().map(|bnode| unsafe { Shared::new(mem::transmute(&bnode)) }))
+    }
+
+    unsafe fn resolve_mut<'a>(&mut self) -> Option<&'a mut Node<T>> {
+        self.0.as_ref().map(|p| &mut ***p)
+    }
+}
+
+
+fn link_no_prev<T>(mut next: Box<Node<T>>) -> Link<T> {
+    next.prev = RawLink::none();
+    Some(next)
 }
