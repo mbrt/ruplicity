f21d0669ab71f0c3d062216e70349d436b67d1ca
Michele Bertasi <michele.bertasi@gmail.com>
2016-03-13 18:42:20 +0100

    Added tests on BlockCache and fixed bugs

--- src/read/cache.rs
+++ src/read/cache.rs
@@ -8,6 +8,7 @@ use signatures::EntryId;
 
 pub type BlockId = (EntryId, usize);
 
+#[derive(Debug)]
 pub struct BlockCache {
     // map from index to block
     // all blocks must be indexed, even unused
@@ -15,6 +16,7 @@ pub struct BlockCache {
     max_blocks: usize,
 }
 
+#[derive(Debug)]
 struct Block(Vec<u8>);
 
 
@@ -63,6 +65,10 @@ impl BlockCache {
         let block = index.get_mut(&id).unwrap();
         block.write_max_block(buffer).ok()
     }
+
+    pub fn clear(&self) {
+        self.index.write().unwrap().clear();
+    }
 }
 
 
@@ -81,8 +87,8 @@ impl Block {
 
     fn write_max_block(&mut self, buffer: &[u8]) -> io::Result<usize> {
         let buffer = &buffer[0..cmp::min(buffer.len(), BLOCK_SIZE)];
-        let len = buffer.len();
-        self.0.write_all(buffer).map(|_| len)
+        self.0.clear();
+        self.0.write(buffer)
     }
 }
 
@@ -101,6 +107,64 @@ mod test {
         assert_eq!(cache.write(id, b"pippo"), Some(5));
         assert_eq!(cache.read(id, &mut buf), Some(5));
         assert_eq!(&buf, b"pippo");
+    }
+
+    #[test]
+    fn max_blocks() {
+        let cache = BlockCache::new(2);
+        let id0 = ((0, 0), 0);
+        let id1 = ((0, 0), 1);
+        let mut buf = vec![0; 3];
+
+        assert_eq!(cache.write(id0, b"id0"), Some(3));
+        assert_eq!(cache.write(id1, b"id1"), Some(3));
+        assert_eq!(cache.read(id0, &mut buf), Some(3));
+        assert_eq!(&buf, b"id0");
+        assert_eq!(cache.read(id1, &mut buf), Some(3));
+        assert_eq!(&buf, b"id1");
+    }
+
+    #[test]
+    fn full_cache() {
+        let cache = BlockCache::new(2);
+        let id0 = ((0, 0), 0);
+        let id1 = ((0, 0), 1);
+        let id2 = ((0, 0), 2);
+        let mut buf = vec![0; 3];
+
+        assert_eq!(cache.write(id0, b"id0"), Some(3));
+        assert_eq!(cache.write(id1, b"id1"), Some(3));
+        assert_eq!(cache.write(id2, b"id2"), Some(3));
+        // id0 disappeared
+        assert_eq!(cache.read(id0, &mut buf), None);
+        // id1 and id2 are there
+        assert_eq!(cache.read(id1, &mut buf), Some(3));
+        assert_eq!(&buf, b"id1");
+        assert_eq!(cache.read(id2, &mut buf), Some(3));
+        assert_eq!(&buf, b"id2");
+    }
+
+    #[test]
+    fn read_refresh_usage() {
+        let cache = BlockCache::new(2);
+        let id0 = ((0, 0), 0);
+        let id1 = ((0, 0), 1);
+        let id2 = ((0, 0), 2);
+        let mut buf = vec![0; 3];
+
+        assert_eq!(cache.write(id0, b"id0"), Some(3));
+        assert_eq!(cache.write(id1, b"id1"), Some(3));
+        // refresh id0, so id1 becomes less used
+        assert_eq!(cache.read(id0, &mut buf), Some(3));
+        // write another
+        assert_eq!(cache.write(id2, b"id2"), Some(3));
+        // id1 disappeared
+        assert_eq!(cache.read(id1, &mut buf), None);
+        // id0 and id2 are there
+        assert_eq!(cache.read(id0, &mut buf), Some(3));
+        assert_eq!(&buf, b"id0");
+        assert_eq!(cache.read(id2, &mut buf), Some(3));
+        assert_eq!(&buf, b"id2");
     }
 
     #[test]
