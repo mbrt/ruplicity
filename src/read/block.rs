f572da58be20cdfa96119a893e86ad0a1aa332e2
Michele Bertasi <michele.bertasi@gmail.com>
2016-07-13 23:30:30 +0200

    Working on BlockProvider
    Better caching for travis ci.

--- src/read/block.rs
+++ src/read/block.rs
@@ -1,6 +1,6 @@
 use signatures::EntryId;
 
-#[allow(dead_code)]
+// max size of a block in volume tars
 pub const BLOCK_SIZE: usize = 64 * 1024;
 
 // Id for entry block ((path, snapshot), block)
--- src/read/mod.rs
+++ src/read/mod.rs
@@ -5,3 +5,33 @@ mod block;
 mod cache;
 #[allow(dead_code)]
 mod volume;
+
+use self::cache::BlockCache;
+use signatures::{Chain, EntryId};
+
+
+pub struct Entry {}
+
+struct BlockProvider {
+    sig: Chain,
+    dcache: BlockCache,
+    scache: BlockCache,
+}
+
+impl BlockProvider {
+    pub fn new(sigchain: Chain, cache_size: usize) -> Self {
+        BlockProvider {
+            sig: sigchain,
+            dcache: BlockCache::new((cache_size as f64 * 0.4) as usize),
+            scache: BlockCache::new((cache_size as f64 * 0.6) as usize),
+        }
+    }
+
+    pub fn signature_chain(&self) -> &Chain {
+        &self.sig
+    }
+
+    pub fn read(&self, entry: EntryId) -> Entry {
+        unimplemented!()
+    }
+}
